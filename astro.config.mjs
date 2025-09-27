7ec4852461da6b5f60b24fa7a901a2a8637c7d83
tam11a <ibrahimsadiktamim@gmail.com>
2025-09-28 02:34:02 +0600

    font, theme config, absolute import added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// TODO: add relative imports for components, layouts, pages, etc.
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: { plugins: [tailwindcss()] },
+});
