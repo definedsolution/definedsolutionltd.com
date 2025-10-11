05e6ebd691b8a823cf741c20a9b23aa15559a392
tam11a <ibrahimsadiktamim@gmail.com>
2025-10-11 06:24:04 +0600

    added: footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // TODO: add relative imports for components, layouts, pages, etc.
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
+  integrations: [
+    icon({
+      include: {
+        lucide: ["*"],
+        "vscode-icons": ["*"],
+      },
+    }),
+  ],
 });
